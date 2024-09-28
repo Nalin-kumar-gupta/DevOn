ef778309e3554716e1b41f3199e155c00ca063e2
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-09-29 02:13:06 +0530

    app2 graph change

--- services/drf_api/views.py
+++ services/drf_api/views.py
@@ -11,10 +11,14 @@ from .logs_sample import all_logs_data
 from .sample_cpu_memory import cpu_memory_data 
 from .data_to_json import extract_and_write_to_json 
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> acdf3fc (sample data sorting function)
 
 class LogsAPIView(APIView):
 =======
+=======
+from .sample_cpu_memory_2 import cpu_memory_data_2
+>>>>>>> 0786ae3 (app2 graph change)
 from .sample_table_data import table_data 
 <<<<<<< HEAD
 class MatricsAPIView(APIView):
@@ -183,17 +187,16 @@ class LogsCaptureAPIView(APIView):
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 =======
         try:
-            # selectedApp
-            # selectedTimeRange
             print(request)
             app_no = request.data.get("selectedApp")
             time_range = request.data.get("selectedTimeRange")
             print(app_no, time_range)
             if not app_no:
                 return Response({"error": "appNo not provided"}, status=status.HTTP_400_BAD_REQUEST)
-            json_data = extract_and_write_to_json(cpu_memory_data)
             if(app_no == 1):
+                json_data = extract_and_write_to_json(cpu_memory_data)
                 return Response(json_data, status=status.HTTP_200_OK)  
+            json_data = extract_and_write_to_json(cpu_memory_data_2)
             return Response(json_data, status=status.HTTP_200_OK) 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
