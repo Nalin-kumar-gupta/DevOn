0111e1e8b7400d4ee67f106c396a0b5e1c1e211c
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-09-29 00:35:40 +0530

    logs call

--- services/drf_api/urls.py
+++ services/drf_api/urls.py
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 from .views import LogsAPIView, LogsCaptureAPIView
+=======
+from .views import LogsAPIView
+from .views import MatricsAPIView
+>>>>>>> dcb10d1 (logs call)
 from django.urls import path, re_path
 
 urlpatterns = [
+    path('matrics/', MatricsAPIView.as_view(), name='get_logs'),
     path('logs/', LogsAPIView.as_view(), name='get_logs'),
     path('capture/', LogsCaptureAPIView.as_view(), name='capture_logs'),
 ]
--- services/drf_api/views.py
+++ services/drf_api/views.py
@@ -10,9 +10,14 @@ from .logs_sample import all_logs_data
 =======
 from .sample_cpu_memory import cpu_memory_data 
 from .data_to_json import extract_and_write_to_json 
+<<<<<<< HEAD
 >>>>>>> acdf3fc (sample data sorting function)
 
 class LogsAPIView(APIView):
+=======
+from .sample_table_data import table_data 
+class MatricsAPIView(APIView):
+>>>>>>> dcb10d1 (logs call)
 
     def post(self, request):
         """
@@ -188,4 +193,20 @@ class LogsCaptureAPIView(APIView):
             return Response(json_data, status=status.HTTP_200_OK) 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+<<<<<<< HEAD
 >>>>>>> acdf3fc (sample data sorting function)
+=======
+        
+class LogsAPIView(APIView):
+
+    def get(self, request):
+        """
+        Get all logs data.
+        """
+        try: 
+            return Response(table_data , status=status.HTTP_200_OK) 
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+>>>>>>> dcb10d1 (logs call)
