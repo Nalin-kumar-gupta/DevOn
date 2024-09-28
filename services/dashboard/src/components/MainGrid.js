ea4855d548bded125b74b86e43c71f9e2988a17c
Nitin0000 <nitinmittal778@gmail.com>
2024-09-28 11:45:33 +0530

    Dashboard Initiation

---
+++ services/dashboard/src/components/MainGrid.js
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid2";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Copyright from "../internals/components/Copyright";
+import ChartUserByCountry from "./ChartUserByCountry";
+import CustomizedTreeView from "./CustomizedTreeView";
+import CustomizedDataGrid from "./CustomizedDataGrid";
+import HighlightedCard from "./HighlightedCard";
+import PageViewsBarChart from "./PageViewsBarChart";
+import SessionsChart from "./SessionsChart";
+import StatCard from "./StatCard";
+
+const data = [
+  {
+    title: "Packages Shipped",
+    value: "14k",
+    interval: "Last 30 days",
+    trend: "up",
+    data: [
+      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340,
+      380, 360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
+    ],
+  },
+  {
+    title: "Visualizations",
+    value: "325",
+    interval: "Last 30 days",
+    trend: "up",
+    data: [
+      220, 300, 360, 400, 480, 520, 500, 840, 620, 660, 740, 380, 760, 800, 780,
+      820, 600, 840, 820, 920, 450, 900, 1080, 720, 900, 1050, 1130, 970, 1250,
+      1640,
+    ],
+  },
+  {
+    title: "Bill",
+    value: "$524",
+    interval: "Last 30 days",
+    trend: "up",
+    data: [
+      300, 900, 600, 1200, 1500, 1800, 2400, 2100, 2700, 3000, 1800, 3300, 3600,
+      3900, 4200, 4500, 3900, 4800, 5100, 5400, 4800, 5700, 6000, 6300, 6600,
+      6900, 7200, 7500, 7800, 8100,
+    ],
+  },
+];
+
+export default function MainGrid() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
+      {/* cards */}
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Billing Overview
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        columns={12}
+        sx={{ mb: (theme) => theme.spacing(2) }}
+      >
+        {data.map((card, index) => (
+          <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
+            <StatCard {...card} />
+          </Grid>
+        ))}
+        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+          <HighlightedCard />
+        </Grid>
+        <Grid size={{ sm: 12, md: 6 }}>
+          <SessionsChart />
+        </Grid>
+        <Grid size={{ sm: 12, md: 6 }}>
+          <PageViewsBarChart />
+        </Grid>
+      </Grid>
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Details
+      </Typography>
+      <Grid container spacing={2} columns={12}>
+          <CustomizedDataGrid />
+      </Grid>
+    </Box>
+  );
+}
