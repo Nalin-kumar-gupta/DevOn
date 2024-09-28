dcb10d10010e3ed92b4774f584638bf10f6aa0ae
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-09-29 00:35:40 +0530

    logs call

--- services/drf_api/urls.py
+++ services/drf_api/urls.py
@@ -1,6 +1,8 @@
 from .views import LogsAPIView
+from .views import MatricsAPIView
 from django.urls import path, re_path
 
 urlpatterns = [
+    path('matrics/', MatricsAPIView.as_view(), name='get_logs'),
     path('logs/', LogsAPIView.as_view(), name='get_logs'),
 ]
--- services/drf_api/views.py
+++ services/drf_api/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .sample_cpu_memory import cpu_memory_data 
 from .data_to_json import extract_and_write_to_json 
-
-class LogsAPIView(APIView):
+from .sample_table_data import table_data 
+class MatricsAPIView(APIView):
 
     def post(self, request):
         """
@@ -26,3 +26,16 @@ class LogsAPIView(APIView):
             return Response(json_data, status=status.HTTP_200_OK) 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class LogsAPIView(APIView):
+
+    def get(self, request):
+        """
+        Get all logs data.
+        """
+        try: 
+            return Response(table_data , status=status.HTTP_200_OK) 
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
